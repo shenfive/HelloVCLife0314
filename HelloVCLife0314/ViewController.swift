991ebd0dc5dc0affe2d0c2ea5ba4ccfd4ab63652
Danny Shen <shenfive@gmail.com>
2020-03-14 13:32:52 +0800

    UIView

--- HelloVCLife0314/ViewController.swift
+++ HelloVCLife0314/ViewController.swift
@@ -11,30 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     var timer:Timer!
-    var counter = 1
+    var counter = 0
     
-    override func loadView() {
-        super.loadView()
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(setCounter(sender:)), userInfo: nil, repeats: true)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("View Will Disappear")
-    }
-
-    @objc func setCounter(sender: Any?){
-        print(counter)
-        counter += 1
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        var newView = UIView.init(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        newView.backgroundColor = UIColor.red
+        self.view.addSubview(newView)
+        
     }
     
-    @IBAction func stopCounter(_ sender: Any) {
-        timer.invalidate()
-    }
+
 }
 
